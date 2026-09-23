aca8229863208f7fb922175243deb3886665efc2
Juan Tedjaatmadja <jetedjaatmadja@gmail.com>
2026-09-23 14:19:23 +0200

    Array toevoegen voor Projecten

---
+++ projecten.js
@@ -0,0 +1,17 @@
+const projecten = [
+    {
+        id: 1,
+        naam: "Databases",
+        beschrijving: "Klassendiagrammen, RRM's en SQL queries"
+    },
+    {
+        id: 2,
+        naam: "Vang de volger",
+        beschrijving: "Een spel waarbij je de volger probeert te vangen."
+    }
+    {
+        id: 3,
+        naam: "Conway's Game of Life",
+        beschrijving: "Een cellulaire automaat die de evolutie van cellen simuleert."
+    }
+];
